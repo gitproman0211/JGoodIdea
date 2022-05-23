e67e81965a78cc09530d5be39232ea428b042913
gitproman0211 <wproman0211@outlook.com>
2022-05-23 14:27:03 -0400

    data update

--- src/app/profile/education/education.component.ts
+++ src/app/profile/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-education',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.scss'],
 })
 export class EducationComponent implements OnInit {
-  constructor() {}
+  educations: any;
 
-  ngOnInit() {}
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit() {
+    this.educations = this.profileService.education();
+  }
 }
